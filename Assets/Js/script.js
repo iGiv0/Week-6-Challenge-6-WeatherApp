f331d2e185c1ece92dc8edb3b12db0db48e89569
iGiv0 <gebrahimi9851@gmail.com>
2023-01-26 23:08:35 -0800

    modified my HTML as well as my JS

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,38 +1,42 @@
 
 var ApiKey = '6ccefdb6dca275a254442e1392a36775'
-var searchCity = document.querySelector('#city-search');
+var searchCity = document.querySelector('#search-city');
 var currentWeather = document.querySelector('#currentweather');
-var searchBtn = document.querySelector('search-button');
-var clearBtn = document.querySelector('clear-button');
-var currentCity = document.querySelector('current-city');
-var currentTemp = document.querySelector('current-temp');
-var currentHumid = document.querySelector('current-humidity');
-var currentWindSpeed = document.querySelector('current-wind');
+var searchBtn = document.querySelector('#search-button');
+var form = document.querySelector('#form');
+var clearBtn = document.querySelector('#clear-button');
+var currentCity = document.querySelector('#current-city');
+var currentTemp = document.querySelector('#current-temp');
+var currentHumid = document.querySelector('#current-humidity');
+var currentWindSpeed = document.querySelector('#current-wind');
+var unorderList = document.querySelector('#group-lists');
 // var currentIcon = ;
 var dayjs = dayjs();
-
+console.log(searchBtn);
 function searchCity(event) {
+    console.log(event);
     event.preventDefault()
-    fetch("api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=6ccefdb6dca275a254442e1392a36775")
-    var cityWeather = search.value
+    console.log('hello');
+    var cityWeather = searchCity.value
+    console.log(cityWeather);
     weatherSearch(cityWeather);
 }
 
-function weatherSearch(city) {
-
-
+function weatherSearch(city) { 
+    console.log(city);
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${ApiKey}`)    
+    
     .then(function(response) {
         return response.json();
     })
-
+    
     .then(function(data) {
-
-        currentCity.textContent = data.name
+        console.log(data);
+        
+        fetch("api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=6ccefdb6dca275a254442e1392a36775")
+        // currentCity.textContent = data.name
         
     })
 }
+searchBtn.addEventListener('click', searchCity);
 
-$('date-1').textContent = dayjs.format('dddd')
-$('temp-1').textContent = 
-$('humidity-1').textContent = 
-$('wind-1').textContent 
